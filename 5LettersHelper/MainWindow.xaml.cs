cc48bc8d8c05fa66dc305520647bdfef4965b4f3
Coupsoul <slaw.kapustin2000@gmail.com>
2025-09-07 21:44:29 +0500

    Initial commit: First release of 5 Letters Helper

---
+++ 5LettersHelper/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace _5LettersHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly WordFilterService _filterService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _filterService = new WordFilterService();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string mask = TextBox1_Mask.Text.ToLowerInvariant();
+            if (mask.Length != 5)
+            {
+                System.Windows.MessageBox.Show("Маска слова должна состоять из 5 символов.", "Ошибка ввода", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            string excludedLetters = TextBox2_ExcludedLetters.Text.ToLowerInvariant();
+            string includedLetters = TextBox3_IncludedLetters.Text.ToLowerInvariant();
+
+            List<string> finalWords = _filterService.GetFilteredWords(mask, excludedLetters, includedLetters);
+
+            Border border = (Border)FinalWordsList.Template.FindName("TextureBorder", FinalWordsList);
+            if (border != null)
+            {
+                ImageBrush? backgroundTexture = border.Background as ImageBrush;
+                if (backgroundTexture != null)
+                {
+                    if (finalWords.Count > 30)
+                    {
+                        FinalWordsList.FontSize = 26;
+                        backgroundTexture.Viewport = new Rect(0, 0, 52, 52);
+                    }
+                    else
+                    {
+                        FinalWordsList.FontSize = 38;
+                        backgroundTexture.Viewport = new Rect(55, 0, 78, 78);
+                    }
+                }
+            }
+
+            FinalWordsList.ItemsSource = finalWords;
+        }
+
+        private void TextBox_Mask_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsLetter(e.Text, 0) && e.Text != "*")
+                e.Handled = true;
+        }
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsLetter(e.Text, 0))
+                e.Handled = true;
+        }
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+    }
+}
---
+++ 5LettersHelper/WordFilterService.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+
+
+namespace _5LettersHelper
+{
+    class WordFilterService
+    {
+        private readonly List<string>? _allWords;
+
+        public WordFilterService()
+        {
+            try
+            {
+                _allWords = File.ReadAllLines("FiveLettersWords.txt").ToList();
+            }
+            catch (Exception ex)
+            {
+                _allWords = new List<string>();
+                System.Windows.MessageBox.Show($"Ошибка загрузки словаря: {ex.Message}");
+            }
+        }
+
+        public List<string> GetFilteredWords(string mask, string excludedLetters, string includedLetters)
+        {
+            var filteredByMask = FilterByMask(_allWords, mask);
+            var filteredByExcluded = FilterByExcludedLetters(filteredByMask, excludedLetters);
+            var finalWords = FilterByIncludedLetters(filteredByExcluded, includedLetters);
+
+            return finalWords;
+        }
+
+        private List<string> FilterByMask(List<string> words, string mask)
+        {
+            if (string.IsNullOrWhiteSpace(mask))
+                return words;
+
+            var result = new List<string>();
+            foreach (string word in words)
+            {
+                bool isMatch = true;
+                for (int i = 0; i < mask.Length; i++)
+                {
+                    if (mask[i] != '*' && mask[i] != word[i])
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch)
+                    result.Add(word);
+            }
+            return result;
+        }
+
+        private List<string> FilterByExcludedLetters(List<string> words, string excludedLetters)
+        {
+            if (string.IsNullOrWhiteSpace(excludedLetters))
+                return words;
+
+            var excludedChars = excludedLetters.ToCharArray();
+            return words.Where(word => !excludedChars.Any(excludedChar => word.Contains(excludedChar))).ToList();
+        }
+
+        private List<string> FilterByIncludedLetters(List<string> words, string includedLetters)
+        {
+            if (string.IsNullOrWhiteSpace(includedLetters))
+                return words;
+
+            var includedChars = includedLetters.ToCharArray();
+            return words.Where(word => includedChars.All(includedChar =>  word.Contains(includedChar))).ToList();
+        }
+    }
+}
